f7839ced05938062cba565aa0fdc17674ac1f3df
Raghav Ramesh <raghavramesh94@gmail.com>
2016-11-15 06:50:47 +0800

    Added negative number validation; Fixed display label bug;

--- popup.js
+++ popup.js
@@ -1,17 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
+  var displayTimeout;
   var saveButton = document.getElementById('saveBtn');
   saveButton.addEventListener('click', () => {
+    if (document.getElementById("intervalField").value < 0) {
+      document.getElementById("displayLabel").innerHTML = "Invalid interval value.";
+      document.getElementById("intervalField").value = 15;
+      displayTimeout = setTimeout(() => {
+        document.getElementById("displayLabel").innerHTML = "";
+      }, 3000);
+
+      return;
+    }
+
     var options = {
       title: document.getElementById("titleField").value || 'Hey!',
       message: document.getElementById("messageField").value || 'Are you working?',
-      interval: document.getElementById("intervalField").value,
+      interval: document.getElementById("intervalField").value || 15,
     }
     console.log(options);
     document.getElementById("titleField").value = "";
     document.getElementById("intervalField").value = "";
     document.getElementById("messageField").value = "";
     document.getElementById("displayLabel").innerHTML = "Settings saved! You can exit the popup now.";
-    setTimeout(() => {
+    if (displayTimeout) clearTimeout(displayTimeout);
+    displayTimeout = setTimeout(() => {
       document.getElementById("displayLabel").innerHTML = "";
     }, 3000);
     chrome.runtime.sendMessage({
@@ -28,7 +40,8 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log(console.log("Response from script:", response.message));
     });
     document.getElementById("displayLabel").innerHTML = "No more notifications will be sent.";
-    setTimeout(() => {
+    if (displayTimeout) clearTimeout(displayTimeout);
+    displayTimeout = setTimeout(() => {
       document.getElementById("displayLabel").innerHTML = "";
     }, 3000);
   });
